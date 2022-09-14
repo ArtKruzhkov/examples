0250ef1974b802d5c7aa12b7a695e9c35de28be4
Artem <artkruzhkov@gmail.com>
2022-09-14 17:45:12 +0300

    Example005IfElse

---
+++ Example005_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("enter your name");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "masha")
+{
+    Console.WriteLine("yey, welcome Masha");
+}
+else
+{
+    Console.Write("hello,");
+    Console.WriteLine(username);
+}
