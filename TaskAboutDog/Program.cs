0da87ddaf33e335d8cf2eaef3120bb5dbd607408
Artem <artkruzhkov@gmail.com>
2022-09-14 19:59:38 +0300

    TaskAboutDog

---
+++ TaskAboutDog/Program.cs
@@ -0,0 +1,24 @@
+﻿int distance = 10000;
+int fFS = 2;
+int sFS = 1;
+int DS = 5;
+int f = 2;
+int count = 0;
+
+while(distance > 10)
+{
+    int time;
+    if(f == 1)
+    {
+        time = distance / (DS+fFS);
+        f = 2;
+    }
+    else
+    {
+        time = distance / (DS+sFS);
+        f = 1;
+    }
+    distance = distance - (fFS+sFS) * time;
+    count = count + 1;
+}
+Console.WriteLine(count);
